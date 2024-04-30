2bff82c72099897477ef3da52e2afec477c11cdd
Jimintae <162279174+hackercatx86@users.noreply.github.com>
2024-04-30 17:04:50 +0900

    week09
    finish

---
+++ src/com/section02/Ex1003.java
@@ -0,0 +1,15 @@
+package com.section02;
+
+import com.javamaster.mypackage.*;
+
+public class Ex1003 {
+
+	public static void main(String[] args) {
+		Cat myCat = new Cat();
+		myCat.eat();
+		myCat.scratch();
+		myCat.meow();
+
+	}
+
+}
